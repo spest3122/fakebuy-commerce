d939c81840967ca715532917b7f84c12d060aac7
spest3122 <spest3122@gmail.com>
2024-02-06 11:03:28 +0800

    feature 1. 半完成nav 2. 增加套件hero.icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <Header></Header>
-  <Main></Main>
+  <div class="min-h-full">
+    <Nav></Nav>
+    <Header></Header>
+    <Main></Main>
+  </div>
 </template>
 <script>
 import Header from "./Header.vue";
 import Main from "./Main.vue";
+import Nav from "./Nav.vue";
 export default {
   components: {
     Header,
     Main,
+    Nav,
   },
 };
 </script>
---
+++ src/views/Nav.vue
@@ -0,0 +1,93 @@
+<template>
+  <nav class="bg-lime-950">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 items-center justify-between">
+        <div class="flex items-center bg-lime-50 p-2 rounded-full">
+          <div class="flex-shrink-0">
+            <img class="h-8 w-8" src="../assets/Flogo.png" alt="fakebuy-logo" />
+          </div>
+        </div>
+        <div class="md:block">
+          <div class="flex items-center md:ml-6">
+            <button
+              type="button"
+              class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            >
+              <span class="absolute -inset-1.5" />
+              <span class="sr-only">View notifications</span>
+              <ShoppingCartIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+
+            <!-- Profile dropdown -->
+            <div class="relative ml-3">
+              <div>
+                <button
+                  class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  @click="togglePanel"
+                >
+                  <span class="absolute -inset-1.5" />
+                  <span class="sr-only">Open user menu</span>
+                  <UserIcon class="h-6 w-6" />
+                </button>
+              </div>
+              <transition
+                enter-active-class="transition ease-out duration-100"
+                enter-from-class="transform opacity-0 scale-95"
+                enter-to-class="transform opacity-100 scale-100"
+                leave-active-class="transition ease-in duration-75"
+                leave-from-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95"
+              >
+                <div
+                  v-if="openPanel"
+                  class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="user-menu-button"
+                  tabindex="-1"
+                >
+                  <!-- Active: "bg-gray-100", Not Active: "" -->
+                  <a
+                    href="#"
+                    class="block px-4 py-2 text-sm text-gray-700"
+                    role="menuitem"
+                    tabindex="-1"
+                    id="user-menu-item-0"
+                    >Your Profile</a
+                  >
+                  <a
+                    href="#"
+                    class="block px-4 py-2 text-sm text-gray-700"
+                    role="menuitem"
+                    tabindex="-1"
+                    id="user-menu-item-1"
+                    >Settings</a
+                  >
+                  <a
+                    href="#"
+                    class="block px-4 py-2 text-sm text-gray-700"
+                    role="menuitem"
+                    tabindex="-1"
+                    id="user-menu-item-2"
+                    >Sign out</a
+                  >
+                </div>
+              </transition>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<script setup>
+import { ref } from "vue";
+import { ShoppingCartIcon, UserIcon } from "@heroicons/vue/24/outline";
+
+const openPanel = ref(false);
+
+function togglePanel() {
+  openPanel.value = !openPanel.value;
+}
+</script>
+<style lang=""></style>
